f526e4c2990dba3a272a4c1d88279429eca73767
Manjunatha Sai Uppu <manjunatha16.512@gmail.com>
2020-05-17 16:07:39 +0530

    Update moviepicker.py

--- moviepicker.py
+++ moviepicker.py
@@ -1,19 +1,20 @@
+# Importing modules which will be used by the project
 import random
 import requests
 from bs4 import BeautifulSoup
-
+# url for getting the movies
 url = 'https://www.imdb.com/chart/top'
-
+# Main Function
 def main():
-    response = requests.get(url)
-    html = response.text
+    response = requests.get(url) #requesting the url
+    html = response.text # accessing the url page as a text
 
     soup = BeautifulSoup(html,'html.parser')
     innermovietags = soup.select('td.titleColumn a')
     movietags = soup.select('td.titleColumn')
     ratings = soup.select('td.posterColumn span[name=ir]')
 
-
+    # The function below helps you to return the year of the movie released
     def get_year(movie_tag):
         movie_split =  movie_tag.text.split()
         year = movie_split[-1]
@@ -30,7 +31,7 @@ def main():
 
         user_input = (input("Another Movie (y/n)")).lower()
         if user_input != 'y':
-            break
+            break  # if user input is yes then it will give you one more movie name
 
 if __name__ == '__main__':
     main()
